1883c92cdb16d23ec47e0165a587ad4ce4112697
macrat <m@crat.jp>
2016-11-26 19:33:57 +0900

    Replaced level images with random images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 
 import cupy
 import cv2
@@ -8,40 +9,48 @@ import converter
 import finder
 
 
-async def take_level_images(cam: finder.Camera, width: int, height: int, step: int = 51, loop: int = 10):
+def make_random_imgee(size):
+    img = numpy.zeros([*size, 3], numpy.uint8)
+
+    for i in range(1000):
+        r = random.randint(0, int(max(size) / ((i+1)/4)))
+        cv2.circle(img, (random.randint(-r, size[1] + r), random.randint(-r, size[0] + r)), r, [random.randint(0, 256) for i in range(3)], -1)
+
+    return img
+
+
+async def take_level_images(cam: finder.Camera, width: int, height: int, num: int = 50):
     taker = finder.SimpleImagePhotoTaker(cam)
     taker.prepare()
-
-    inputs = [
-        numpy.ones((height, width, 3), numpy.uint8) * i
-        for i in range(0, 255, step)
-    ]
 
     await finder.wait_for_return()
 
     results = []
-    for i in range(loop):
-        for j, img in enumerate(inputs):
-            id_ = i * len(inputs) + j
+    inputs = []
+    for i in range(num):
+        print('{0:.1%}'.format(i / num))
 
-            print('{0:.1%}'.format(id_ / (loop * len(inputs))))
+        img = make_random_imgee((height, width))
 
-            result = await taker.take_async(img, 500)
+        result = await taker.take_async(img, 300)
 
-            cv2.imwrite('out/show/{0:04}.png'.format(id_), img)
-            cv2.imwrite('out/saw/{0:04}.png'.format(id_), result)
+        cv2.imwrite('out/show/{0:04}.png'.format(i), img)
+        cv2.imwrite('out/saw/{0:04}.png'.format(i), result)
 
-            results.append(result)
+        inputs.append(img)
+        results.append(result)
 
-    return numpy.array(inputs * loop), numpy.array(results)
+    return numpy.array(inputs), numpy.array(results)
 
 
 async def main(cam: finder.Camera, width: int, height: int):
     pcam = await finder.PerspectiveCamera.auto_create(cam, width, height)
 
-    ys, xs = await take_level_images(pcam, width, height, step=51, loop=10)
+    ys, xs = await take_level_images(pcam, width, height)
 
+    print('make linear function...')
     f = converter.LinearFunction.from_least_squares(cupy.array(xs), cupy.array(ys))
+    print('done')
 
     import pickle
     with open('f.pickle', 'wb') as fp:
@@ -60,6 +69,6 @@ async def main(cam: finder.Camera, width: int, height: int):
 
 
 if __name__ == '__main__':
-    cam = finder.CameraMonitor(1)
+    cam = finder.CameraMonitor()
     cam.set_mode(1280, 800)
     asyncio.get_event_loop().run_until_complete(asyncio.wait([cam.mainloop(), main(cam, 1680, 1050)]))
